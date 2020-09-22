8c28a97c9df104c66adc612ad8fbde2785865aed
saraivafelipe27 <saraivafelipe27@gmail.com>
2020-09-21 21:24:48 -0300

    code update

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-const express = require('express');
+// const express = require('express');
 
-const app = express();
+// const app = express();
 
-app.get('/', (request, response) => {
-    return response.json({ message: 'Olá felp' });
-});
+// app.get('/', (request, response) => {
+//     return response.json({ message: 'Olá felp' });
+// });
 
-app.listen(3333, () => {
-    console.log('Back-end started!')
-});
+// app.listen(3333, () => {
+//     console.log('Back-end started!')
+// });
