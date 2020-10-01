b51881864e694af7ef9e7026e836883ecf33801c
saraivafelipe27 <saraivafelipe27@gmail.com>
2020-10-01 16:51:34 -0300

    Update code

--- src/index.js
+++ src/index.js
@@ -1,11 +1,54 @@
-// const express = require('express');
+const express = require('express');
 
-// const app = express();
+const app = express();
 
-// app.get('/', (request, response) => {
-//     return response.json({ message: 'Olá felp' });
-// });
+app.use(express.json());
 
-// app.listen(3333, () => {
-//     console.log('Back-end started!')
-// });
+app.get('/projects', (request, response) => {
+    const { title, owner } = request.query;
+
+    console.log(title);
+    console.log(owner);
+
+    return response.json([
+        'Project 1',
+        'Project 2'
+    ]);
+});
+
+
+app.post('/projects', (request, response) => {
+    const { title, owner} = request.body;
+
+    console.log(title);
+    console.log(owner);
+
+    return response.json([
+        'Project 1',
+        'Project 2',
+        'Project 3'
+    ]);
+});
+
+app.put('/projects/:id', (request, response) => {
+    const { id } = request.params;
+    
+    console.log(id);
+
+    return response.json([
+        'Project 4',
+        'Project 2',
+        'Project 3'
+    ]);
+});
+
+app.delete('/projects/:id', (request, response) => {
+    return response.json([
+        'Project 2',
+        'Project 3'
+    ]);
+});
+
+app.listen(3333, () => {
+    console.log('Back-end started!')
+});
